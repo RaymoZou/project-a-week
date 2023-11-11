9bf67fb03477d2541dbaf508e653d03e76bc7687
Raymond Zou <raymond.1zou@gmail.com>
2023-11-10 16:41:11 -0800

    add jwt signing and verification

--- Week 1 - Clicker Game/server/src/index.ts
+++ Week 1 - Clicker Game/server/src/index.ts
@@ -1,12 +1,51 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express, { NextFunction, Request, Response } from 'express';
 import { router } from './router';
+import jwt from 'jsonwebtoken';
 dotenv.config();
 
 const app = express();
-app.use('/api', router)
+app.use(express.json());
 
-console.log('sd');
+const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    try {
+        if (!token) return res.status(401).json({ message: 'no jwt token provided' });
+        jwt.verify(token, process.env.SECRET_KEY as string, (err, decoded) => {
+            if (err) throw err;
+        });
+        next();
+    } catch (err) {
+        next(err);
+        return res.status(401).json({ message: "you are not authenticated" });
+    }
+};
+
+app.post('/login', (req: Request, res: Response) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // TODO: implement storing username + password hash on mongoDB with bcrypt
+    // verify user credentials
+    // for now: only valid credentials are {username: user, password: pass}
+
+    if (username === 'user' && password === 'pass') {
+        var token = jwt.sign({ username }, process.env.SECRET_KEY as string, {
+            // 1 hour
+            expiresIn: 60 * 60
+        });
+        res.cookie("jwt", token);
+        res.status(200).json({ message: 'login success' });
+    } else {
+        res.status(401).json({ message: 'login failure' });
+    }
+});
+
+app.use('/api', router)
+app.get('/auth', isAuthenticated, (req: Request, res: Response, next) => {
+    res.json('this is authorized route');
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`listening on port ${process.env.PORT || 3000}`);
