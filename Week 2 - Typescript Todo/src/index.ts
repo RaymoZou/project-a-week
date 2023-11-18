c2ffd16410b93cb3ce9753ffb386747d09633aad
Raymond Zou <raymond.1zou@gmail.com>
2023-11-17 21:47:14 -0800

    add db connection method

--- Week 2 - Typescript Todo/src/index.ts
+++ Week 2 - Typescript Todo/src/index.ts
@@ -1,4 +1,6 @@
+import 'dotenv/config';
 import inquirer from 'inquirer';
+import pg from 'pg';
 
 const promptUser = async () => {
     const answer = await inquirer.prompt([{
@@ -8,21 +10,45 @@ const promptUser = async () => {
         choices: ['Create a new TODO', 'View all TODOs'],
     },
     ]);
+    return answer.action;
 
-    switch (answer.action) {
-        case 'Create a new TODO':
-            console.log('creating a new todo');
-            break;
-        case 'View all TODOs':
-            console.log('Viewing all TODOs');
-            break;
-        default:
-            console.error('Unexpected error has occurred, try again');
-            promptUser();
+    // switch (answer.action) {
+    //     case 'Create a new TODO':
+    //         console.log('creating a new todo');
+    //         break;
+    //     case 'View all TODOs':
+    //         console.log('Viewing all TODOs');
+    //         break;
+    //     default:
+    //         console.error('Unexpected error has occurred, try again');
+    //         promptUser();
+    // };
+};
+
+const connect_database = async () => {
+    try {
+        console.log('Connecting to database...');
+        const client = new pg.Client();
+        await client.connect();
+        console.log('Connected!');
+        return client;
+    } catch (err) {
+        throw new Error('There was an error connecting to the database...');
     };
 };
 
-promptUser();
+const main = async () => {
+    try {
+        const client = await connect_database();
+        const action = await promptUser();
+        console.log(action);
+        client.end();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+main();
 
 
 
