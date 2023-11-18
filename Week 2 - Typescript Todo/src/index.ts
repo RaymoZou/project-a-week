66b58ff7bbaad675f5f4bd1341866d01d244bf8c
Raymond Zou <raymond.1zou@gmail.com>
2023-11-18 12:13:27 -0800

    add get todo function

--- Week 2 - Typescript Todo/src/index.ts
+++ Week 2 - Typescript Todo/src/index.ts
@@ -1,50 +1,86 @@
 import 'dotenv/config';
 import inquirer from 'inquirer';
-import pg from 'pg';
+import pg, { Client } from 'pg';
 
-const promptUser = async () => {
+var client: Client;
+
+const getInput = async (client: Client) => {
     const answer = await inquirer.prompt([{
         type: 'list',
         name: 'action',
         message: 'What would you like to do?',
-        choices: ['Create a new TODO', 'View all TODOs'],
+        choices: ['Create a new TODO', 'View all TODOs', 'Quit'],
     },
     ]);
-    return answer.action;
 
-    // switch (answer.action) {
-    //     case 'Create a new TODO':
-    //         console.log('creating a new todo');
-    //         break;
-    //     case 'View all TODOs':
-    //         console.log('Viewing all TODOs');
-    //         break;
-    //     default:
-    //         console.error('Unexpected error has occurred, try again');
-    //         promptUser();
-    // };
+    return answer.action;
 };
 
 const connect_database = async () => {
     try {
         console.log('Connecting to database...');
-        const client = new pg.Client();
+        client = new pg.Client();
         await client.connect();
         console.log('Connected!');
         return client;
     } catch (err) {
-        throw new Error('There was an error connecting to the database...');
+        throw err;
     };
 };
 
+const printTodos = async () => {
+    try {
+        const todos = await client.query(`SELECT * FROM todos`);
+        todos.rows.forEach(todo => {
+            console.log(`------------------`)
+            console.log(`ID: ${todo.id}\nTitle: ${todo.title}`);
+            console.log(`------------------`)
+        })
+    } catch (err) {
+        throw err;
+        // throw new Error('There was an error getting the TODOs');
+    }
+};
+
+const initiate_db = async (client: Client) => {
+    try {
+        await client.query(
+            `CREATE TABLE todos (
+                id serial PRIMARY KEY,
+                title text
+            )`
+        );
+    } catch (err) {
+        throw err;
+    }
+}
+
 const main = async () => {
     try {
+        // connect to local postgreSQL database
         const client = await connect_database();
-        const action = await promptUser();
-        console.log(action);
-        client.end();
+
+        // enter cli loop
+        while (true) {
+            const action = await getInput(client);
+            switch (action) {
+                case 'Create a new TODO':
+                    console.log('TODO: implement create new TODO function');
+                    break;
+                case 'View all TODOs':
+                    await printTodos();
+                    break;
+                case 'Quit':
+                    break;
+                default:
+                    console.error('Unexpected error has occurred, try again');
+            };
+            if (action === 'Quit') break;
+        }
     } catch (err) {
         console.error(err);
+    } finally {
+        await client.end();
     }
 };
 
