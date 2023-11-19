31426e0d651d328efdeb0e80805fa89b69ce4678
Raymond Zou <raymond.1zou@gmail.com>
2023-11-18 17:50:49 -0800

    add option to initiate and clear database

--- Week 2 - Typescript Todo/src/index.ts
+++ Week 2 - Typescript Todo/src/index.ts
@@ -1,14 +1,16 @@
 import 'dotenv/config';
 import inquirer from 'inquirer';
 import pg, { Client } from 'pg';
+import { v4 as uuidv4 } from 'uuid';
 
 var client: Client;
 
 const choices = [
-    'Create a new TODO ✏️',
-    'View all TODOs 🔍',
-    'Quit 🚪',
-
+    'Create a new TODO',
+    'View all TODOs',
+    'Initiate Database',
+    'Clear Database',
+    'Quit',
 ];
 
 const getInput = async () => {
@@ -37,15 +39,14 @@ const connectDB = async () => {
 
 const printTodos = async () => {
     try {
-        const todos = await client.query(`SELECT * FROM todos`);
+        const todos = await client.query(`SELECT * FROM todos;`);
         todos.rows.forEach(todo => {
             console.log(`------------------`)
-            console.log(`ID: ${todo.id}\nTitle: ${todo.title}`);
+            console.log(`ID: ${todo.id}\nTitle: ${todo.title}\nDescription: ${todo.description}`);
             console.log(`------------------`)
         })
     } catch (err) {
         throw err;
-        // throw new Error('There was an error getting the TODOs');
     }
 };
 
@@ -53,13 +54,41 @@ const initiateDB = async () => {
     try {
         await client.query(
             `CREATE TABLE todos (
-                id serial PRIMARY KEY,
-                title text
-            )`
-        );
+                id text PRIMARY KEY,
+                title text,
+                description text
+            );`
+        );  
     } catch (err) {
         throw err;
     }
+}
+
+const createTodo = async () => {
+    try {
+        const todo = await inquirer.prompt([{
+            type: 'input',
+            name: 'title',
+            message: 'Enter TODO title',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter TODO description',
+        }]);
+        const id = uuidv4();
+        console.log(id, todo.title, todo.description);
+        client.query({
+            text: 'INSERT INTO todos (id, title, description) VALUES ($1, $2, $3)',
+            values: [id, todo.title, todo.description]
+        });
+    } catch (err) {
+        throw err;
+    }
+}
+
+const resetTodos = async () => {
+    await client.query(`DROP TABLE todos;`);
 }
 
 const main = async () => {
@@ -72,13 +101,19 @@ const main = async () => {
             const action = await getInput();
             switch (action) {
                 case choices[0]:
-                    console.log('TODO: implement create new TODO function');
+                    await createTodo();
                     break;
                 case choices[1]:
                     await printTodos();
                     break;
+                case choices[2]:
+                    await initiateDB();
+                    break;
+                case choices[3]:
+                    await resetTodos();
+                    break;
             };
-            if (action === choices[2]) break;
+            if (action === choices[choices.length - 1]) break;
         }
     } catch (err) {
         console.error(err);
