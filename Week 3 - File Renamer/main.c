81c3d04dcf7511533fb97d640d28a7c8576c35ed
Raymond Zou <raymond.1zou@gmail.com>
2023-11-22 22:55:19 -0800

    add basic print statements

--- Week 3 - File Renamer/main.c
+++ Week 3 - File Renamer/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("hello world");
+int main(int argc, char *argv[]) {
+    printf("Name of this program: %s\n", argv[0]);
+    printf("The number of arguments passed was: %d\n", argc);
+    // argv is a pointer to character array
     return 0;
 }
