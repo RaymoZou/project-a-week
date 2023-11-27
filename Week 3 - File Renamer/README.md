9172498d7c841cc5efb1ff449654f8e8709bef71
Raymond Zou <raymond.1zou@gmail.com>
2023-11-26 23:59:08 -0800

    add scanner for initial user action

--- Week 3 - File Renamer/README.md
+++ Week 3 - File Renamer/README.md
@@ -3,4 +3,6 @@
 Week 3 of PAW. Diving into the world of C...
 
 ## Features
-- Store and retrieve string values in memory
+- Store and retrieve string values in memory 
+- Enter 'v' in CLI to view strings
+- All other comamnds will be interpreted as adding a string
--- Week 3 - File Renamer/main.c
+++ Week 3 - File Renamer/main.c
@@ -13,27 +13,33 @@ int main() {
     char *pointer_arr[MAX_STRING_SIZE];
     char *temp_pointer;
 
+    char curr_command[MAX_BUFFER_SIZE];
+
     while (1) {
-        printf("Enter string:\n");
-        scanf("%s", char_buffer);
+        printf("Enter command:\n- (v)iew strings\n");
+        scanf("%s", curr_command);
+        if (strcmp(curr_command, "v") == 0) {
+            // print all the strings that have been stored so far
+            for (int i = 0; i < curr_index; i++) {
+                // printf("%d\n", i);
+                printf("Item stored at index %d: %s\n", i, pointer_arr[i]);
+            };
+        } else {
+            printf("Enter string:\n");
+            scanf("%s", char_buffer);
 
-        // if size of string exceeds MAX_BUFFER_SIZE, return error
-        if ((strlen(char_buffer) + 1) > MAX_BUFFER_SIZE) {
-            fprintf(stderr, "Error reading input.\n");
-            return -1;
-        };
+            // if size of string exceeds MAX_BUFFER_SIZE, return error
+            if ((strlen(char_buffer) + 1) > MAX_BUFFER_SIZE) {
+                fprintf(stderr, "Error reading input.\n");
+                return -1;
+            };
 
-        temp_pointer = malloc((strlen(char_buffer) + 1) * sizeof(char));
-        strcpy(temp_pointer, char_buffer);
-        pointer_arr[curr_index] = temp_pointer;
+            temp_pointer = malloc((strlen(char_buffer) + 1) * sizeof(char));
+            strcpy(temp_pointer, char_buffer);
+            pointer_arr[curr_index] = temp_pointer;
 
-        // print all the strings that have been stored so far
-        for (int i = 0; i <= curr_index; i++) {
-            // printf("%d\n", i);
-            printf("Item stored at index %d: %s\n", i, pointer_arr[i]);
-        };
-
-        curr_index++;
+            curr_index++;
+        }
     };
 
     return 0;
