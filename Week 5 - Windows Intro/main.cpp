63c80d0cf3c3c6e01c393d76db2f2c94b2d08837
Raymond Zou <raymond.1zou@gmail.com>
2023-12-10 14:15:18 -0800

    add bouncing box animation

--- Week 5 - Windows Intro/main.cpp
+++ Week 5 - Windows Intro/main.cpp
@@ -1,21 +1,72 @@
+#include <d2d1.h>
 #include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM laparam);
+
+const int UPDATE_TIME = 1;
+const int WINDOW_LENGTH = 200;
+const int WINDOW_HEIGHT = 200;
+const int X_MAX = GetSystemMetrics(SM_CXSCREEN);
+const int X_MIN = 0;
+const int Y_MAX = GetSystemMetrics(SM_CYSCREEN);
+const int Y_MIN = 0;
+int X_VELOCITY = 10;
+int Y_VELOCITY = 10;
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PWSTR pCmdLine,
+                    int nCmdShow) {
+    WNDCLASSW wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"Testing";
+    if (!RegisterClassW(&wc)) return -1;
+
+    HWND hwnd = CreateWindowW(wc.lpszClassName, L"Cool Window Name",
+                              WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0,
+                              WINDOW_LENGTH, WINDOW_HEIGHT, 0, 0, hInstance, 0);
+    if (!hwnd) return -1;
+
+    SetTimer(hwnd, 0, UPDATE_TIME, 0);
+
+    MSG msg = {};
+    while (GetMessageW(&msg, 0, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    };
+
+    return 0;
+};
+
+int MoveWindow(HWND hwnd) {
+    RECT currPos = {};
+    if (GetWindowRect(hwnd, &currPos) == 0) return -1;
+    if (currPos.bottom + Y_VELOCITY > Y_MAX | currPos.top + Y_VELOCITY < Y_MIN) {
+        Y_VELOCITY = -Y_VELOCITY;
+    };
+    if (currPos.right + X_VELOCITY > X_MAX | currPos.left + X_VELOCITY < X_MIN) {
+        X_VELOCITY = -X_VELOCITY;
+    }
+
+    int newY = currPos.top + Y_VELOCITY;
+    int newX = currPos.left + X_VELOCITY;
+
+    // bottom boundary
+    SetWindowPos(hwnd, 0, newX, newY, WINDOW_LENGTH, WINDOW_HEIGHT, 0);
+
+    return 0;
+};
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
     switch (msg) {
         case WM_CLOSE:
             DestroyWindow(hwnd);
             break;
+        case WM_TIMER: {
+            MoveWindow(hwnd);
+            break;
+        }
         case WM_DESTROY:
             PostQuitMessage(0);
-            break;
-        case WM_LBUTTONDOWN:
-            OutputDebugStringW(L"What up");
-            break;
-        case WM_TIMER:
-            OutputDebugStringW(L"Timer has expired....");
-            // SetWindowPos(hwnd, 0, 100, 100, 300, 300, 0);
-            RECT dimensions;
-            GetWindowRect(hwnd, &dimensions);
             break;
         case WM_PAINT: {
             // create PAINTSTRUCT
@@ -37,45 +88,5 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
             return DefWindowProcW(hwnd, msg, wparam, lparam);
             break;
     };
-    return 0;
-};
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PWSTR pCmdLine,
-                    int nCmdShow) {
-    WNDCLASSW wc = {};
-    // wc.style = CS_HREDRAW | CS_VREDRAW;
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = L"Testing";
-
-    // RegisterClassW will return 0 if function call fails
-    if (!RegisterClassW(&wc)) {
-        MessageBoxW(NULL, L"Window Registration Failed!", L"ERROR",
-                    MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    };
-
-    // Create Window (CreateWindowW)
-    HWND hwnd = CreateWindowW(wc.lpszClassName, L"Cool Window Name",
-                              WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 800, 800,
-                              0, 0, hInstance, 0);
-
-    if (hwnd == NULL) {
-        MessageBoxW(NULL, L"Window Creation Failed", L"ERROR",
-                    MB_ICONEXCLAMATION | MB_OK);
-        return -1;
-    };
-
-    // Create a 3 second timer
-    SetTimer(hwnd, 0, 1000, 0);
-
-    ShowWindow(hwnd, nCmdShow);
-
-    MSG msg = {};
-    while (GetMessageW(&msg, 0, 0, 0) > 0) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    };
-
     return 0;
 };
