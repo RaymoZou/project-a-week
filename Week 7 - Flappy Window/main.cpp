6217fb0f8439b3a8e07bd0f3fb740ac534a7270e
Raymond Zou <raymond.1zou@gmail.com>
2023-12-20 22:22:36 -0800

    add WM_CLOSE handler

--- Week 7 - Flappy Window/main.cpp
+++ Week 7 - Flappy Window/main.cpp
@@ -1,11 +1,34 @@
 #include <windows.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+    switch (msg) {
+        case WM_CLOSE:
+            PostQuitMessage(0);
+            return 0;
+        case WM_TIMER:
+            OutputDebugStringW(L"Testing");
+    };
     return DefWindowProcW(hwnd, msg, wparam, lparam);
-}
+};
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                     PWSTR pCmdLine, int nCmdShow) {
-    WNDCLASSEXW wc = {};
+    WNDCLASSW wc = {0};
+    wc.lpfnWndProc = &WindowProc;
+    wc.lpszClassName = L"Testing";
+
+    RegisterClassW(&wc);
+    HWND hwnd = CreateWindowW(wc.lpszClassName, L"Cool Window Name",
+                              WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 800, 800,
+                              0, 0, hInstance, 0);
+    if (!hwnd) return -1;
+
+    // SetTimer(hwnd, 0, 1000, 0);
+
+    MSG msg = {};
+    while (GetMessageW(&msg, 0, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    };
     return 0;
 };
