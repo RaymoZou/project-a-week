1e6645c1fd71e9e9c2f75bea22ffe4d26ec0f667
Raymond Zou <raymond.1zou@gmail.com>
2023-12-22 21:38:33 -0800

    add main window class

--- Week 7 - Flappy Window/main.cpp
+++ Week 7 - Flappy Window/main.cpp
@@ -1,28 +1,35 @@
 #include <windows.h>
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-    switch (msg) {
-        case WM_CLOSE:
-            PostQuitMessage(0);
-            return 0;
-        case WM_TIMER:
-            OutputDebugStringW(L"Testing");
+class MainWindow {
+   public:
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam,
+                                       LPARAM lparam) {
+        switch (msg) {
+            case WM_CLOSE:
+                PostQuitMessage(0);
+                return 0;
+            case WM_TIMER:
+                OutputDebugStringW(L"Testing");
+        };
+        return DefWindowProcW(hwnd, msg, wparam, lparam);
     };
-    return DefWindowProcW(hwnd, msg, wparam, lparam);
+
+    MainWindow(HINSTANCE hInstance) {
+        WNDCLASSW wc = {0};
+        wc.lpfnWndProc = &WindowProc;
+        wc.lpszClassName = L"Testing";
+
+        RegisterClassW(&wc);
+        HWND hwnd = CreateWindowW(wc.lpszClassName, L"Cool Window Name",
+                                  WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 800,
+                                  800, 0, 0, hInstance, 0);
+    };
 };
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                     PWSTR pCmdLine, int nCmdShow) {
-    WNDCLASSW wc = {0};
-    wc.lpfnWndProc = &WindowProc;
-    wc.lpszClassName = L"Testing";
 
-    RegisterClassW(&wc);
-    HWND hwnd = CreateWindowW(wc.lpszClassName, L"Cool Window Name",
-                              WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 800, 800,
-                              0, 0, hInstance, 0);
-    if (!hwnd) return -1;
-
+    MainWindow mainWindow = MainWindow(hInstance);
     // SetTimer(hwnd, 0, 1000, 0);
 
     MSG msg = {};
