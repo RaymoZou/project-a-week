6742560dc40c6fb1b358185350cb9bad54225384
Raymond Zou <raymond.1zou@gmail.com>
2023-12-23 22:06:56 -0800

    change class name initiation

--- Week 7 - Flappy Window/main.cpp
+++ Week 7 - Flappy Window/main.cpp
@@ -2,36 +2,77 @@
 
 class MainWindow {
    public:
+    HWND hwnd;
     static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam,
                                        LPARAM lparam) {
         switch (msg) {
             case WM_CLOSE:
-                PostQuitMessage(0);
-                return 0;
+                DestroyWindow(hwnd);
+                break;
             case WM_TIMER:
-                OutputDebugStringW(L"Testing");
+                OutputDebugStringW(L"WM_TIMER from Main\n");
+                break;
+            case WM_DESTROY:
+                PostQuitMessage(0);
+            default:
+                return DefWindowProcW(hwnd, msg, wparam, lparam);
         };
-        return DefWindowProcW(hwnd, msg, wparam, lparam);
+        return 0;
     };
 
     MainWindow(HINSTANCE hInstance) {
         WNDCLASSW wc = {0};
         wc.lpfnWndProc = &WindowProc;
-        wc.lpszClassName = L"Testing";
+        wc.lpszClassName = L"Main Window";
 
         RegisterClassW(&wc);
-        HWND hwnd = CreateWindowW(wc.lpszClassName, L"Cool Window Name",
-                                  WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 800,
-                                  800, 0, 0, hInstance, 0);
+        hwnd = CreateWindowW(wc.lpszClassName, L"Main",
+                             WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 800, 800,
+                             0, 0, hInstance, 0);
+    };
+};
+
+class PlayerWindow {
+   public:
+    HWND hwnd;
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam,
+                                       LPARAM lparam) {
+        switch (msg) {
+            case WM_CLOSE:
+                DestroyWindow(hwnd);
+                break;
+            case WM_TIMER:
+                OutputDebugStringW(L"WM_TIMER from Player\n");
+                break;
+            case WM_DESTROY:
+                // Do nothing - quit message only sent from Main Window
+            default:
+                return DefWindowProcW(hwnd, msg, wparam, lparam);
+        };
+        return 0;
+    };
+
+    PlayerWindow(HINSTANCE hInstance) {
+        WNDCLASSW wc = {0};
+        wc.lpfnWndProc = &WindowProc;
+        wc.lpszClassName = L"Player Window";
+
+        RegisterClassW(&wc);
+        hwnd = CreateWindowW(wc.lpszClassName, L"Player",
+                             WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 800, 800,
+                             0, 0, hInstance, 0);
     };
 };
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                     PWSTR pCmdLine, int nCmdShow) {
-
     MainWindow mainWindow = MainWindow(hInstance);
-    // SetTimer(hwnd, 0, 1000, 0);
+    PlayerWindow playerWindow = PlayerWindow(hInstance);
 
+    SetTimer(mainWindow.hwnd, 0, 1000, 0);
+    SetTimer(playerWindow.hwnd, 0, 1000, 0);
+
+    // Run message loop
     MSG msg = {};
     while (GetMessageW(&msg, 0, 0, 0) > 0) {
         TranslateMessage(&msg);
