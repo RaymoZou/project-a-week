35fab30f2a0d57d6a66dfc9933c7787d87c7daeb
Raymond Zou <raymond.1zou@gmail.com>
2023-12-24 22:46:28 -0800

    preserve active player window

--- Week 7 - Flappy Window/main.cpp
+++ Week 7 - Flappy Window/main.cpp
@@ -48,16 +48,18 @@ class PipeWindow {
     };
 
     PipeWindow() {
-        WNDCLASSW wc = {0};
+        WNDCLASSEXW wc = {0};
+        wc.cbSize = sizeof(WNDCLASSEXW);
+        // wc.cbWndExtra = DLGWINDOWEXTRA;
         wc.lpfnWndProc = &WindowProc;
         wc.lpszClassName = L"Pipe Window";
         wc.hInstance = GetModuleHandle(NULL);
 
-        RegisterClassW(&wc);
-        hwnd = CreateWindowW(wc.lpszClassName, L"Pipe",
-                             WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-                             screenWidth - WINDOW_LENGTH, 0, WINDOW_LENGTH,
-                             WINDOW_HEIGHT, 0, 0, GetModuleHandle(NULL), 0);
+        RegisterClassExW(&wc);
+        hwnd = CreateWindowExW(WS_EX_NOACTIVATE, wc.lpszClassName, L"Pipe",
+                               WS_VISIBLE | WS_DISABLED,
+                               screenWidth - WINDOW_LENGTH, 0, WINDOW_LENGTH,
+                               WINDOW_HEIGHT, 0, 0, GetModuleHandle(NULL), 0);
 
         // Set pipe velocity
         SetTimer(hwnd, 0, update_time, 0);
@@ -85,14 +87,15 @@ class MainWindow {
     };
 
     MainWindow(HINSTANCE hInstance) {
-        WNDCLASSW wc = {0};
+        WNDCLASSEXW wc = {0};
+        wc.cbSize = sizeof(WNDCLASSEXW);
         wc.lpfnWndProc = &WindowProc;
         wc.lpszClassName = L"Main Window";
 
-        RegisterClassW(&wc);
-        hwnd = CreateWindowW(wc.lpszClassName, L"Main",
-                             WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 800, 800,
-                             0, 0, hInstance, 0);
+        RegisterClassExW(&wc);
+        hwnd = CreateWindowExW(0, wc.lpszClassName, L"Main",
+                               WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 800, 800,
+                               0, 0, hInstance, 0);
         // Set pipe spawn rate
         SetTimer(hwnd, 0, pipe_spawn_rate, 0);
     };
@@ -105,23 +108,6 @@ class PlayerWindow {
 
    public:
     HWND hwnd;
-
-    // int MovePlayer() {
-    //     // get current window position
-    //     RECT playerPos;
-    //     if (GetWindowRect(hwnd, &playerPos) == 0) {
-    //         return -1;
-    //         // TODO: handle error
-    //     };
-
-    //     velocity = velocity + gravity_scale;
-    //     if (MoveWindow(hwnd, playerPos.left, playerPos.top + velocity,
-    //                    WINDOW_LENGTH, WINDOW_HEIGHT, FALSE) == 0)
-    //         // TODO: handle error
-    //         return -1;
-
-    //     return 0;
-    // };
 
     static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam,
                                        LPARAM lparam) {
@@ -149,6 +135,7 @@ class PlayerWindow {
                 // Do nothing - quit message only sent from Main Window
             case WM_KEYDOWN:
                 if (wparam == VK_SPACE) {
+                    OutputDebugStringW(L"Space was pressed.\n");
                     velocity = jumpForce;
                 };
                 break;
@@ -159,20 +146,24 @@ class PlayerWindow {
     };
 
     PlayerWindow() {
-        WNDCLASSW wc = {0};
+        WNDCLASSEXW wc = {0};
+        wc.cbSize = sizeof(WNDCLASSEXW);
         wc.lpfnWndProc = &WindowProc;
         wc.lpszClassName = L"Player Window";
         wc.hInstance = GetModuleHandle(NULL);
 
-        RegisterClassW(&wc);
-        hwnd = CreateWindowW(
-            wc.lpszClassName, L"Player", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0,
-            WINDOW_LENGTH, WINDOW_HEIGHT, 0, 0, GetModuleHandle(NULL), 0);
+        RegisterClassExW(&wc);
+        hwnd = CreateWindowExW(
+            0, wc.lpszClassName, L"Player", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0,
+            0, WINDOW_LENGTH, WINDOW_HEIGHT, 0, 0, GetModuleHandle(NULL), 0);
     };
 };
 
-TIMERPROC CreatePipeProc(HWND hwnd, UINT msg, UINT_PTR idEvent, DWORD dwTime) {
+VOID CALLBACK PipeTimerProc(HWND hwnd, UINT msg, UINT_PTR idEvent,
+                            DWORD dwTime) {
+    HWND playerHWND = GetActiveWindow();
     PipeWindow pipeWindow = PipeWindow();
+    SetActiveWindow(playerHWND);
 };
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
@@ -181,7 +172,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     // PipeWindow pipeWindow = PipeWindow();
     PlayerWindow playerWindow = PlayerWindow();
 
-    SetTimer(0, 1, pipe_spawn_rate, CreatePipe);
+    SetTimer(0, 1, pipe_spawn_rate, PipeTimerProc);
     SetTimer(playerWindow.hwnd, 0, update_time, 0);
     // Run message loop
     MSG msg = {};
