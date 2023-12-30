67da2258de979c192b436f634fc959d4779614ae
Raymond Zou <raymond.1zou@gmail.com>
2023-12-30 09:35:25 -0800

    initiate vite project

--- Week 8 - RGB Color Guesser/index.js
+++ Week 8 - RGB Color Guesser/index.js
@@ -1,19 +1,28 @@
-var red;
-var blue;
-var green;
+let red;
+let blue;
+let green;
 
 const getColor = () => {
-    red = Math.floor(Math.random() * 256);
-    green = Math.floor(Math.random() * 256);
-    blue = Math.floor(Math.random() * 256);
-    color_div = document.getElementById('color');
-    color_div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+	red = Math.floor(Math.random() * 256);
+	green = Math.floor(Math.random() * 256);
+	blue = Math.floor(Math.random() * 256);
+	color_div = document.getElementById("color");
+	color_div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 };
 
 const handleClick = () => {
-    document.getElementById('form').addEventListener('submit', (e) => {
-        e.preventDefault();
-    console.log("click handled...)});
+	// console.log("handling click...");
+	redGuess = document.getElementById("red").value;
+	blueGuess = document.getElementById("blue").value;
+	greenGuess = document.getElementById("green").value;
+	console.log(redGuess / red);
+	console.log(blueGuess / blue);
+	console.log(greenGuess / green);
 };
+
+document.getElementById("form").addEventListener("submit", (e) => {
+	e.preventDefault();
+	// console.log(redGuess / red);
+});
 
 getColor();
