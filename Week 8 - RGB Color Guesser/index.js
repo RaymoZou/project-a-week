5fc7bd1c0d4f90bee7e1163c5a5ddd333cbb0338
Raymond Zou <raymond.1zou@gmail.com>
2023-12-29 20:30:58 -0800

    add form

--- Week 8 - RGB Color Guesser/index.js
+++ Week 8 - RGB Color Guesser/index.js
@@ -1,9 +1,19 @@
+var red;
+var blue;
+var green;
+
 const getColor = () => {
-    var red = Math.floor(Math.random() * 256);
-    var green = Math.floor(Math.random() * 256);
-    var blue = Math.floor(Math.random() * 256);
+    red = Math.floor(Math.random() * 256);
+    green = Math.floor(Math.random() * 256);
+    blue = Math.floor(Math.random() * 256);
     color_div = document.getElementById('color');
     color_div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+};
+
+const handleClick = () => {
+    document.getElementById('form').addEventListener('submit', (e) => {
+        e.preventDefault();
+    console.log("click handled...)});
 };
 
 getColor();
