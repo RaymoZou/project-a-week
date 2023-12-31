426cfb9063c0c6929c4fb5911e94818949f8a714
Raymond Zou <raymond.1zou@gmail.com>
2023-12-30 18:52:53 -0800

    add tailwindcss

--- Week 8 - RGB Color Guesser/index.js
+++ Week 8 - RGB Color Guesser/index.js
@@ -1,28 +1,28 @@
-let red;
-let blue;
-let green;
-
-const getColor = () => {
-	red = Math.floor(Math.random() * 256);
-	green = Math.floor(Math.random() * 256);
-	blue = Math.floor(Math.random() * 256);
-	color_div = document.getElementById("color");
-	color_div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-};
-
-const handleClick = () => {
-	// console.log("handling click...");
-	redGuess = document.getElementById("red").value;
-	blueGuess = document.getElementById("blue").value;
-	greenGuess = document.getElementById("green").value;
-	console.log(redGuess / red);
-	console.log(blueGuess / blue);
-	console.log(greenGuess / green);
-};
-
-document.getElementById("form").addEventListener("submit", (e) => {
-	e.preventDefault();
-	// console.log(redGuess / red);
-});
-
-getColor();
+// let red;
+// let blue;
+// let green;
+//
+// const getColor = () => {
+// 	red = Math.floor(Math.random() * 256);
+// 	green = Math.floor(Math.random() * 256);
+// 	blue = Math.floor(Math.random() * 256);
+// 	color_div = document.getElementById("color");
+// 	color_div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+// };
+//
+// const handleClick = () => {
+// 	// console.log("handling click...");
+// 	redGuess = document.getElementById("red").value;
+// 	blueGuess = document.getElementById("blue").value;
+// 	greenGuess = document.getElementById("green").value;
+// 	console.log(redGuess / red);
+// 	console.log(blueGuess / blue);
+// 	console.log(greenGuess / green);
+// };
+//
+// document.getElementById("form").addEventListener("submit", (e) => {
+// 	e.preventDefault();
+// 	// console.log(redGuess / red);
+// });
+//
+// getColor();
