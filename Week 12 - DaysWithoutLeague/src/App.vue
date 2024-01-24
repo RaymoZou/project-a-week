11d37ec6320b494a85d4d901d8b3821b3c37da5b
Raymond Zou <raymond.1zou@gmail.com>
2024-01-24 12:02:38 -0800

    add rendering of last played game

--- Week 12 - DaysWithoutLeague/src/App.vue
+++ Week 12 - DaysWithoutLeague/src/App.vue
@@ -1,38 +1,39 @@
 <!-- using the Options API  -->
-<script>
+<script lang="ts">
 import axios from 'axios';
+import { fromUnixTime } from 'date-fns'
 
 export default {
 
 	data() {
 		return {
-			games: null
+			lastGame: null
 		}
 	},
 
 	computed: {
-		numGames() {
-			return this.data ? this.data.data.length : "not loaded";
-		}
+		// convert unix time to readable format
+		lastPlayed() { return this.lastGame ? fromUnixTime(1705876957690 / 1000) : "NOT LOADED" }
 	},
 
 	methods: {
 		async getData() {
-			this.games = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${import.met.env.PUUID}/ids?start=0&count=20&api_key=${import.meta.env.VITE_RITO_API}`)
-		}
+		// TODO: user input of PUUID (currently set in .env)
+			const response = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${import.meta.env.VITE_PUUID}/ids?start=0&count=1&api_key=${import.meta.env.VITE_RITO_API}`)
+			const matchId = response.data
+			const gameInfo = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${import.meta.env.VITE_RITO_API}`)
+			this.lastGame = gameInfo.data
+		},
 	},
 
 	mounted() {
-		// console.log(import.meta.env.VITE_RITO_API)
 		this.getData()
-		console.log("mounted")
 	},
 }
 </script>
 
 <template>
-	<h1>Games</h1>
-	<li v-for="game in games.data">
-		{{ game }}
-	</li>
+	<h1>Last League of Legends Game</h1>
+	<!-- <p>last game created (in unix time): {{ lastGame ? lastGame.info.gameCreation : "loading..." }}</p> -->
+	<p><strong>Last game of League of Legends played on:</strong> {{ lastPlayed }}</p>
 </template>
