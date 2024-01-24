5f30240a6c54f2b4071013e4ef60b8ab4f008169
raymond <raymond.1zou@gmail.com>
2024-01-23 22:12:53 -0800

    add api call test

--- Week 12 - DaysWithoutLeague/src/App.vue
+++ Week 12 - DaysWithoutLeague/src/App.vue
@@ -1,7 +1,38 @@
 <!-- using the Options API  -->
-export () => {
+<script>
+import axios from 'axios';
+
+export default {
+
+	data() {
+		return {
+			games: null
+		}
+	},
+
+	computed: {
+		numGames() {
+			return this.data ? this.data.data.length : "not loaded";
+		}
+	},
+
+	methods: {
+		async getData() {
+			this.games = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${import.met.env.PUUID}/ids?start=0&count=20&api_key=${import.meta.env.VITE_RITO_API}`)
+		}
+	},
+
+	mounted() {
+		// console.log(import.meta.env.VITE_RITO_API)
+		this.getData()
+		console.log("mounted")
+	},
 }
+</script>
 
 <template>
-	hello world from App.vue
+	<h1>Games</h1>
+	<li v-for="game in games.data">
+		{{ game }}
+	</li>
 </template>
