6cb159215c64f54ce72ec71a15ec34e1210e53cc
Raymond Zou <raymond.1zou@gmail.com>
2024-01-26 23:52:10 -0800

    add entry for riot api key

--- Week 12 - DaysWithoutLeague/src/App.vue
+++ Week 12 - DaysWithoutLeague/src/App.vue
@@ -11,31 +11,48 @@ export default {
 			puuid: null,
 			gameName: null,
 			tagLine: null,
+			apiStatus: false,
+			riotApi: null,
 		}
 	},
 
 	computed: {
-		// convert unix time to readable format
-		// TODO: replace with actual time
 		lastPlayed() { return this.lastGame ? fromUnixTime(this.lastGame.info.gameCreation / 1000) : "NOT LOADED" }
 	},
 
 	methods: {
 		async getData() {
-		// TODO: game name + tag line -> puuid -> last game played
-			// this.puuid = await axios.get(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${this.gameName}/${this.tagLine}?api_key=${import.meta.env.VITE_RITO_API}`)
-			// var test = await axios.get(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/vwls/hehe?api_key=RGAPI-f1851ec3-61dc-47f2-b14a-11b534e8ff24`)
-			const response = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${this.puuid}/ids?start=0&count=1&api_key=${import.meta.env.VITE_RITO_API}`)
+			// TODO: fix api query by game name + tag line
+			// const response = await axios.get(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${this.gameName}/${this.tagLine}?api_key=${import.meta.env.VITE_RITO_API}`)
+			const response = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${this.puuid}/ids?start=0&count=1&api_key=${this.riotApi}`)
 			const matchId = response.data
-			const gameInfo = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${import.meta.env.VITE_RITO_API}`)
+			const gameInfo = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${this.riotApi}`)
 			this.lastGame = gameInfo.data
 			// console.log(gameInfo.data.info.gameCreation)
 		},
 
-		handleSubmit(e: Event) {
-			e.preventDefault()
+		handle_puuid_submit(e: Event) {
+			e.preventDefault();
 			// gameName and tagLine should != null at this point
 			this.getData();
+		},
+
+		async handle_api_submit(e: Event) {
+			e.preventDefault();
+			// check response status is === 200
+			console.log(this.riotApi)
+			try {
+				const response = await axios.get(`https://na1.api.riotgames.com/lol/status/v4/platform-data?api_key=${this.riotApi}`)
+				if (response.status === 200) {
+					this.apiStatus = true;
+					// TODO: set cookie
+				} else {
+					console.log('nope')
+				}
+			} catch (err) {
+				// TODO: proper error handling
+				console.log(err)
+			}
 		}
 	},
 
@@ -44,13 +61,18 @@ export default {
 
 <template>
 	<h1>Last League of Legends Game</h1>
-	<!-- <p>last game created (in unix time): {{ lastGame ? lastGame.info.gameCreation : "loading..." }}</p> -->
-	<form @submit="handleSubmit">
+	<p v-if="apiStatus">You are ready to make an API request!</p>
+	<p v-else="apiStatus">Input your Riot API </p>
+	<form v-else="apiStatus" @submit="handle_api_submit">
+		<input type="text" v-model="riotApi" placeholder="RGAPI-XXXXXXXX-...">
+		<button>submit</button>
+	</form>
+	<form v-if="apiStatus" @submit="handle_puuid_submit">
 		<input type="text" v-model="puuid" placeholder="puuid" required>
 		<!-- <input type="text" v-model="gameName" placeholder="gameName" required> -->
 		<!-- <input type="text" v-model="tagLine" placeholder="tagLine" required> -->
 		<button>submit</button>
 	</form>
-	<p><strong>Last game of League of Legends played on:</strong> {{ lastPlayed }}</p>
-	<p>{{ puuid ? puuid : "puid not loaded" }}</p>
+	<p v-if="apiStatus"><strong>Last game of League of Legends played on:</strong> {{ lastPlayed }}</p>
+	<p v-if="apiStatus">{{ puuid ? puuid : "puid not loaded" }}</p>
 </template>
