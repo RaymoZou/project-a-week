f581f7dfec103b0a67101cf5225da3f28f88be5b
Raymond Zou <raymond.1zou@gmail.com>
2024-02-02 21:50:24 -0800

    add hello world

--- Week 13 - Raw Node Http Server/server.js
+++ Week 13 - Raw Node Http Server/server.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const PORT = 3000;
+const http = require('node:http')
+
+const server = http.createServer((req, res) => {
+    // send http headers
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // send body data
+    res.write('hello world'); // 11 bytes
+    // terminate response
+    res.end();
+});
+
+// listen on port
+server.listen(PORT, () => { console.log(`Listening on port ${PORT}`) });
