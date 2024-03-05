941e58db661baca92f97dd1bba82d8e58e391814
Raymond Zou <raymond.1zou@gmail.com>
2024-03-05 00:09:33 -0800

    add event loop skeleton

--- Week 18 - Basic Sprite Renderer/main.cpp
+++ Week 18 - Basic Sprite Renderer/main.cpp
@@ -1,6 +1,45 @@
+#include "SDL_error.h"
+#include "SDL_events.h"
+#include "SDL_log.h"
+#include "SDL_render.h"
+#include "SDL_stdinc.h"
+#include "SDL_timer.h"
+#include "SDL_video.h"
+#include <SDL.h>
 #include <cstdio>
 
+#define HEIGHT 400
+#define WIDTH 600
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_bool isRunning = SDL_TRUE;
+
 int main(int argc, char *argv[]) {
-  printf("hello world\n");
+
+  // non-zero on failure
+  int init_result = SDL_Init(SDL_INIT_VIDEO);
+
+  // NULL on failure
+  window =
+      SDL_CreateWindow("Basic Sprite Renderer", 100, 100, WIDTH, HEIGHT, 0);
+
+  if (!window | init_result) {
+    printf("error: %s\n", SDL_GetError());
+    return -1;
+  };
+
+  while (isRunning) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    if (event.type == SDL_QUIT) {
+      isRunning = SDL_FALSE;
+    };
+  };
+
+  SDL_DestroyWindow(window);
+  SDL_DestroyRenderer(renderer);
+  SDL_Quit();
+
   return 0;
 };
