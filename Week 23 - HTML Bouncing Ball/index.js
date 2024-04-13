a76d990a612f88ee08f2a25500cc1c5bfd75be52
Raymond <raymond.1zou@gmail.com>
2024-04-13 15:28:51 -0700

    rename variables

--- Week 23 - HTML Bouncing Ball/index.js
+++ Week 23 - HTML Bouncing Ball/index.js
@@ -7,29 +7,29 @@ const WIDTH = canvas.width;
 const BALL_SIZE = 10;
 const FPS = 60;
 
-var x = WIDTH / 2;
-var y = HEIGHT / 2;
+var ball_x = WIDTH / 2;
+var ball_y = HEIGHT / 2;
 var x_velocity = 2;
 var y_velocity = 2;
 
 function reset() {
     console.info("resetting game");
-    x = WIDTH / 2;
-    y = HEIGHT / 2;
+    ball_x = WIDTH / 2;
+    ball_y = HEIGHT / 2;
 };
 
 function render() {
     ctx.clearRect(0, 0, WIDTH, HEIGHT); // clear the canvas
-    ctx.fillRect(x, y, BALL_SIZE, BALL_SIZE);
+    ctx.fillRect(ball_x, ball_y, BALL_SIZE, BALL_SIZE);
 };
 
 function update() {
-    if (x <= 0 && x_velocity < 0) x_velocity *= -1;
-    if (x >= WIDTH && x_velocity > 0) x_velocity *= -1;
-    if (y <= 0 && y_velocity < 0) y_velocity *= -1;
-    if (y >= HEIGHT && y_velocity) y_velocity *= -1;
-    x += x_velocity;
-    y += y_velocity;
+    if (ball_x <= 0 && x_velocity < 0) x_velocity *= -1;
+    if (ball_x >= WIDTH && x_velocity > 0) x_velocity *= -1;
+    if (ball_y <= 0 && y_velocity < 0) y_velocity *= -1;
+    if (ball_y >= HEIGHT && y_velocity) y_velocity *= -1;
+    ball_x += x_velocity;
+    ball_y += y_velocity;
 };
 
 function runGame() {
