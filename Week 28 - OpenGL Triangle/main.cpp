2d975ad7c559798dcb9782f9d145f71a87c22d1a
Raymond <raymond.1zou@gmail.com>
2024-05-19 11:57:19 -0700

    add fragment shader for rendering color

--- Week 28 - OpenGL Triangle/main.cpp
+++ Week 28 - OpenGL Triangle/main.cpp
@@ -21,20 +21,73 @@ int main() {
   SDL_GL_CreateContext(window);
   SDL_Log("%s\n", glGetString(GL_VERSION));
 
-  if (!glewInit()) {
-    SDL_Log("Error: %s\n", SDL_GetError());
+  if (glewInit() != GLEW_OK) {
+    SDL_Log("Error initializing GLEW library\n");
   };
 
-  float vertices[6] = {-0.5f, 0.0f, 0.0f, 0.5f, 0.5f, 0.0f};
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
   SDL_bool shouldClose = SDL_FALSE;
 
-  glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+  glClearColor(0.2f, 0.2f, 0.0f, 1.0f);
+
+  // vertex shader
+  const char *vertex_shader_source =
+      "#version 330 core\n"
+      "layout (location = 0) in vec3 aPos;\n"
+      "void main()\n"
+      "{\n"
+      "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+      "}\0";
+  const char *frag_shader_src = "#version 330 core\n"
+                                "out vec4 FragColor;\n"
+                                "void main()\n"
+                                "{\n"
+                                "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                "}\0";
+
+  // compile shader
+  unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+  glCompileShader(vertex_shader);
+
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+    SDL_Log("vertex shader compilation failed\n");
+  };
+
+  unsigned int frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(frag_shader, 1, &frag_shader_src, NULL);
+  glCompileShader(frag_shader);
+  glGetShaderiv(frag_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+    SDL_Log("frag shader compilation failed\n");
+  };
+
+  // create program
+  unsigned int shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertex_shader);
+  glAttachShader(shaderProgram, frag_shader);
+  glLinkProgram(shaderProgram);
+
+  // vertex array object (VAO)
+  unsigned int vao, vbo;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
 
   // vertex buffer object (VBO)
-  unsigned int vbo;
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  glDeleteShader(vertex_shader);
+  glDeleteShader(frag_shader);
 
   while (!shouldClose) {
     SDL_Event event;
@@ -43,8 +96,18 @@ int main() {
       shouldClose = SDL_TRUE;
     };
     glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(shaderProgram);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     SDL_GL_SwapWindow(window);
   };
+  /* glClearColor(1.0f, 0.0f, 0.0f, 1.0f); */
+  /* glClear(GL_COLOR_BUFFER_BIT); */
+  /* glUseProgram(shaderProgram); */
+  /* glBindVertexArray(vao); */
+  /* glDrawArrays(GL_TRIANGLES, 0, 3); */
+
+  /* SDL_Delay(2500); */
 
   return 0;
 };
