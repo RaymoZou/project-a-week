59816518368cfd23bafad91958ed303fabbe4633
Raymond <raymond.1zou@gmail.com>
2024-05-30 23:11:10 -0700

    create OpenGL Core context

---
+++ Week 30 - OpenGL Cube/README.md
@@ -0,0 +1,1 @@
+# Week 30 of Paw - Matrix Math Adventures
--- Week 30 - OpenGL Cube/main.cpp
+++ Week 30 - OpenGL Cube/main.cpp
@@ -1,10 +1,21 @@
+#include <SDL2/SDL_log.h>
+#include <SDL2/SDL_video.h>
 #include <glm/ext/vector_float2.hpp>
 #include <glm/fwd.hpp>
 #include <glm/matrix.hpp>
 #include <glm/vec2.hpp>
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
 
 int main() {
+    SDL_Window *window = SDL_CreateWindow("PAW - Week 30", 0, 0, 300, 300, SDL_WINDOW_OPENGL);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3); 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3); 
+    SDL_GL_CreateContext(window);
+    glewInit();
+    SDL_Log("OpenGL version: %s\n", glGetString(GL_VERSION));
+
   // pos:
   // -1
   //  2
