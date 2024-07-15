cdceb3a3ef890b46b9c434b81afe58368ed9dd78
RaymoZou <raymond.1zou@gmail.com>
2024-07-14 22:01:25 -0700

    add double jump

---
+++ Week 34 - Shape Dodger/bullet.gd
@@ -0,0 +1,17 @@
+extends AnimatableBody2D
+
+const SPEED = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+#func _physics_process(delta):
+	#print("hello")
+	#constant_linear_velocity.x -= SPEED
+	
--- Week 34 - Shape Dodger/player.gd
+++ Week 34 - Shape Dodger/player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@export var num_jumps: int = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _init():
+		print("hello world")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,8 +16,12 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and num_jumps < 1:
 		velocity.y = JUMP_VELOCITY
+		num_jumps += 1
+		
+	if is_on_floor():
+		num_jumps = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -26,3 +32,4 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
